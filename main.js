d6e9fcd0d576d2d8704b453582f12f908ac57bb2
rubcub13579 <rubcub13579@gmail.com>
2025-01-27 16:59:27 +0200

    search function added

--- main.js
+++ main.js
@@ -1,51 +1,53 @@
 "use strict";
 
-(()=>{
+(() => {
 
-const coinsContainer = document.getElementById("coinsContainer");
-const searchInput = document.getElementById("searchInput");
-
-
-
-let allCoins = [];
-loadLocalStorage();
-
-async function createCoinsArray() {
-    const coinArray = []
-    let id = 0;
-
-    const coins = await getCoinsData();
-    for (const c of coins) {
-
-        const coinId = id++
-        const coinName = c.id;
-        const coinSymbol = c.symbol.toUpperCase();
-        const coinImage = c.image;
+    const coinsContainer = document.getElementById("coinsContainer");
+    const searchInput = document.getElementById("searchInput");
 
 
-        coinArray.push({ coinId, coinName, coinSymbol, coinImage })
 
+    loadLocalStorage();
+
+
+    async function createCoinsArray() {
+        const coinArray = []
+        let id = 0;
+
+        const coins = await getCoinsData();
+        for (const c of coins) {
+
+            const coinId = id++
+            const coinName = c.id;
+            const coinSymbol = c.symbol.toUpperCase();
+            const coinImage = c.image;
+
+
+            coinArray.push({ coinId, coinName, coinSymbol, coinImage })
+
+
+        }
+
+        return coinArray;
 
     }
 
-    return coinArray;
 
-}
+    function displayCoins(allCoins) {
 
+        coinsContainer.innerHTML = ``;
+        let coins = allCoins;
 
-async function displayCoins(allCoins) {
-    let coins = await allCoins;
+        for (let i = 0; i < coins.length; i++) {
+            const coin = coins[i];
 
-    for (let i = 0; i < coins.length; i++) {
-        const coin = coins[i];
+            // Create the HTML structure for each coin
+            const coinDiv = document.createElement("div");
+            coinDiv.classList.add("coinDiv", "form-switch", "position-relative", "border", "rounded", "p-3", "mb-3");
+            coinDiv.id = `coinDiv${coin.coinId}`;
 
-        // Create the HTML structure for each coin
-        const coinDiv = document.createElement("div");
-        coinDiv.classList.add("coinDiv", "form-switch", "position-relative", "border", "rounded", "p-3", "mb-3");
-        coinDiv.id = `coinDiv${coin.coinId}`;
-
-        // Add the HTML content inside the coinDiv
-        coinDiv.innerHTML += `
+            // Add the HTML content inside the coinDiv
+            coinDiv.innerHTML += `
             <input class="form-check-input position-absolute top-0 end-0 m-2" type="checkbox" role="switch" id="flexSwitchCheckDefault${coin.coinId}">
             <img src="${coin.coinImage}" height="45" class="me-2">
             <b class="me-2">${coin.coinSymbol}</b>
@@ -57,125 +59,187 @@ async function displayCoins(allCoins) {
             </div>
         `;
 
-        // Find the "More Info" button inside this coin div and add event listener
-        const infoButton = coinDiv.querySelector("button");
-        infoButton.addEventListener("click", () => moreInfo(coin.coinId));
+            // Find the "More Info" button inside this coin div and add event listener
+            const infoButton = coinDiv.querySelector("button");
+            infoButton.addEventListener("click", () => moreInfo(coin.coinId));
 
-        // Append the coin div to the container
-        coinsContainer.appendChild(coinDiv);
+            // Append the coin div to the container
+            coinsContainer.appendChild(coinDiv);
+        }
     }
-}
 
 
-async function moreInfo(id) {
+    async function moreInfo(id) {
+
+        const currentPrice = await getCoinsPrice(id);
 
 
-    const currentPrice = await getCoinsPrice(id);
+        if (currentPrice) {
 
-
-
-    const coinPriceDiv = document.getElementById("coinPriceDiv" + id);
-    coinPriceDiv.innerHTML =
-        `
-        <b>
+            const coinPriceDiv = document.getElementById("coinPriceDiv" + id);
+            coinPriceDiv.innerHTML =
+                `
+            <b>
             Dollar: ${currentPrice.usd}
-        <br>
+            <br>
             Euro: ${currentPrice.euro}
-        <br>
+            <br>
             Ils: ${currentPrice.ils}
-        </b>
+            </b>
+            
+            `
+        }
 
-        `
-
-}
-
-
-async function getCoinsPrice(id) {
-
-
-    // const usdUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
-    // const euroUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur";
-    // const ilsUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ils";
-
-    const usdUrl = "jsons/coins.json";
-    const euroUrl = "jsons/europrice.json";
-    const ilsUrl = "jsons/ilsprice.json";
-
-    const responseUsd = await axios.get(usdUrl);
-    const responseEuro = await axios.get(euroUrl);
-    const responseIls = await axios.get(ilsUrl);
-
-    const coinsU = responseUsd.data;
-    const coinsE = responseEuro.data;
-    const coinsI = responseIls.data;
+        else {
+            const coinPriceDiv = document.getElementById("coinPriceDiv" + id);
+            coinPriceDiv.innerHTML =
+                `
+            <b>
+                No Data. 
+                <br>
+                Sorry ☹
+            </b>
+            
+            `
+        };
 
 
-
-    const priceInUsd = coinsU[id].current_price;
-    const priceInEuro = coinsE[id].current_price;
-    const priceInIls = coinsI[id].current_price;
+    }
 
 
-    const currentPrice = { usd: priceInUsd, euro: priceInEuro, ils: priceInIls }
+    async function getCoinsPrice(id) {
 
-
-
-
-
-    return currentPrice;
-
-
-}
-
-
-
-async function getCoinsData() {
-
-    // const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
-
-
-    const tempUrl = "jsons/coins.json"
-    const response = await axios.get(tempUrl);
-    const coins = response.data;
-    return coins;
-
-}
-
-
-async function saveCoinsData(allCoins) {
-
-    const data = await allCoins;
-    const json = JSON.stringify(data);
-    localStorage.setItem("allCoins", json);
-
-
-}
-
-
-async function loadLocalStorage() {
-
-    let json = localStorage.getItem("allCoins");
-    let data = JSON.parse(json);
-
-
-
-    if (!json) {
         try {
-            const allCoins = await createCoinsArray();
-            saveCoinsData(allCoins);
-            loadLocalStorage();
+            const usdUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
+            const euroUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur";
+            const ilsUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ils";
+
+            // const usdUrl = "jsons/coins.json";
+            // const euroUrl = "jsons/europrice.json";
+            // const ilsUrl = "jsons/ilsprice.json";
+
+            const responseUsd = await axios.get(usdUrl);
+            const responseEuro = await axios.get(euroUrl);
+            const responseIls = await axios.get(ilsUrl);
+
+
+
+            const coinsU = responseUsd.data;
+            const coinsE = responseEuro.data;
+            const coinsI = responseIls.data;
+
+
+
+            const priceInUsd = coinsU[id].current_price;
+            const priceInEuro = coinsE[id].current_price;
+            const priceInIls = coinsI[id].current_price;
+
+
+            const currentPrice = { usd: priceInUsd, euro: priceInEuro, ils: priceInIls }
+
+
+
+
+
+            return currentPrice;
         }
         catch (err) {
-            alert("The server is not responding");
-            return;
+            alert("Too many requests. Try Again Later.");
+            return null;
         }
+
     }
 
 
-    if (json)
-        displayCoins(data);
 
-}
+    async function getCoinsData() {
+
+        const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
+
+
+        // const url = "jsons/coins.json"
+        const response = await axios.get(url);
+        const coins = response.data;
+        return coins;
+
+    }
+
+
+    function saveCoinsData(allCoins) {
+
+        const data = allCoins;
+        const json = JSON.stringify(data);
+        localStorage.setItem("allCoins", json);
+
+
+    }
+
+
+    async function loadLocalStorage() {
+
+        let json = localStorage.getItem("allCoins");
+        let data = JSON.parse(json);
+
+
+
+        if (!json) {
+            try {
+                const allCoins = await createCoinsArray();
+                saveCoinsData(allCoins);
+                loadLocalStorage();
+            }
+            catch (err) {
+                alert("The server is not responding");
+                return;
+            }
+        }
+
+
+        if (json)
+            displayCoins(data);
+
+    }
+
+    // ------------------------------------
+
+
+
+    searchInput.addEventListener("input", function () {
+
+
+        const json = localStorage.getItem("allCoins");
+        const coins = JSON.parse(json);
+
+
+        const searchValue = searchInput.value.toLowerCase()
+
+        const coinArray = [];
+
+        for (const c of coins) {
+
+            if (c.coinName.toLowerCase().includes(searchValue) || c.coinSymbol.toLowerCase().includes(searchValue)) {
+                const coinId = c.coinId;
+                const coinName = c.coinName;
+                const coinSymbol = c.coinSymbol;
+                const coinImage = c.coinImage;
+
+                coinArray.push({ coinId, coinName, coinSymbol, coinImage });
+
+            }
+        }
+        
+        if (coinArray.length === 0 && searchValue.trim() !== "") {
+            coinsContainer.innerHTML = `
+            <b>No Coins Are Found</b>
+            `;
+            return;
+        }
+
+        displayCoins(coinArray);
+
+    })
+
+
 
 
 
